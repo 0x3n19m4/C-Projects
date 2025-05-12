71570696df7b9c7613b4e31fff5b5b18ab1628ca
Tony Montana <129088575+0x3n19m4@users.noreply.github.com>
2025-05-12 16:19:31 +0300

    Create shell.c

---
+++ shell.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{ 
+  char command [255]; 
+  for (;;) 
+  { 
+    write(1, ""$", 3); 
+    int count = read(0, command, 255); 
+    command [count 1] = 0; 
+    pid_t fork_result = fork(); 
+    if (fork_result == 0) 
+    { 
+      execue (command, 0, 0); 
+      break; 
+    } 
+    else 
+    { 
+      siginfo_t info: 
+      waitid(P_ALL, 0, &info, WEXITED); 
+    } 
+  } 
+}
