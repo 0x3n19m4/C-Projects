df68d4c1a19632b57b99bc29a7a9df946a71cc23
Tony Montana <129088575+0x3n19m4@users.noreply.github.com>
2025-08-03 00:36:53 +0000

    Create and Update func.c

--- asm/func.asm
+++ asm/func.asm
@@ -4,17 +4,17 @@ section .text
     global _start
 
 _start:
-    mov rdi, 5
-    mov rsi, 5
+    mov rdi, 5   ; firch argument
+    mov rsi, 5   ; second argument
 
     call sum
-    add rax, 10
-
-    mov rdi, rax
-    mov rax, 60
-    syscall
-
-sum:
-    mov rax, rdi
-    add rax, rsi
-    ret    
+    add rax, 10   ; <---------------------------------------------------------------| add 10 to rax(10) = 20
+                  ;                                                                 |
+    mov rdi, rax  ;                                                                 |
+    mov rax, 60   ;                                                                 |
+    syscall       ;                                                                 |
+                  ;                                                                 |
+sum:              ;                                                                 |
+    mov rax, rdi  ;                                                                 |
+    add rax, rsi  ;                                                                 |
+    ret           ; ret instruction will return result from add operation to rax register 
