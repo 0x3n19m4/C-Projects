87eb89677fb2262b29c77e4d04f508b63b44cb2b
Tony Montana <129088575+0x3n19m4@users.noreply.github.com>
2025-08-04 22:18:03 +0300

    Update func.asm

--- asm/func.asm
+++ asm/func.asm
@@ -8,13 +8,13 @@ _start:
     mov rsi, 5   ; second argument
 
     call sum
-    add rax, 10   ; <---------------------------------------------------------------| add 10 to rax(10) = 20
-                  ;                                                                 |
-    mov rdi, rax  ;                                                                 |
-    mov rax, 60   ;                                                                 |
-    syscall       ;                                                                 |
-                  ;                                                                 |
-sum:              ;                                                                 |
-    mov rax, rdi  ;                                                                 |
-    add rax, rsi  ;                                                                 |
+    add rax, 10   ; <===============================================================|| add 10 to rax(10) = 20
+                  ;                                                                 ||
+    mov rdi, rax  ;                                                                 ||
+    mov rax, 60   ;                                                                 ||
+    syscall       ;                                                                 ||
+                  ;                                                                 ||
+sum:              ;                                                                 ||
+    mov rax, rdi  ;                                                                 ||
+    add rax, rsi  ;                                                                 ||
     ret           ; ret instruction will return result from add operation to rax register 
